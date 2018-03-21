f5c094450e7b5ec35dffd6556c43fb159e5d9806
levon0101 <levon.ecs@gmail.com>
2018-03-21 14:30:42 +0400

    FriendControl

---
+++ FriendVIewer/DesignTimeData/DesignFriend.cs
@@ -0,0 +1,34 @@
+﻿using FriendVIewer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FriendVIewer.DesignTimeData
+{
+    public class DesignFriend : Friend  // make it public after chack
+    {
+        public DesignFriend()
+        {
+            FirstName = "Levon";
+            LastName = "Mardanyan";
+            Email = "levon.ecs@gmail.com";
+            HomePage = "none";
+            setImageProperty();
+        }
+
+        private void setImageProperty()
+        {
+            var streemRecourceInfo = Application.GetResourceStream(
+                new Uri("FriendViewer,Component/DesignTimeData/Images/Leovn.jpg", UriKind.Relative));
+
+            var length = streemRecourceInfo.Stream.Length;
+            byte[] image = new byte[length];
+            streemRecourceInfo.Stream.Read(image, 0, (int)length);
+
+            Image = image;
+        }
+    }
+}
---
+++ FriendVIewer/Model/Friend.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendVIewer.Model
+{
+    public class Friend
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string CellPhone { get; set; }
+
+        public string HomePage { get; set; }
+
+        public byte[] Image { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return string.Format($"{FirstName} {LastName}");
+            }
+        }
+    }
+}
